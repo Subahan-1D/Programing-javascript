7a004e426563888ba2020a0642731502c651fae8
Subahan-1D <subahanislam523@gmail.com>
2024-02-09 21:12:17 +0600

    added milestone 4 and 5

---
+++ Milestone -4/module 23- advance/advance.js
@@ -0,0 +1,39 @@
+
+
+const numbers = [ 98,98,97,96,98]
+
+   /* for (let i = 0 ; i < numbers.length ; i++){
+        const number = numbers[i];
+        console.log(number);
+
+    }
+    */
+   // for off loop 
+
+//    for (const number of numbers){
+//     console.log(number)
+//    }
+
+const products =[
+    {id : 1, name : 'Walton Fhone' , price : 20000,},
+    {id : 2, name : 'Walton Fhone' , price : 20000,},
+    {id : 3, name : 'Walton Fhone' , price : 20000,},
+    {id : 4, name : 'Walton Fhone' , price : 20000,},
+    {id : 5, name : 'Walton Fhone' , price : 20000,},
+    {id : 6, name : 'Walton Fhone' , price : 20000,},
+    {id : 7, name : 'Walton Fhone' , price : 20000,},
+    {id : 8, name : 'Walton Fhone' , price : 20000,},
+    {id : 9, name : 'Walton Fhone' , price : 20000,},
+    {id : 9, name : ' I Fhone' , price : 20000,},
+];
+
+for(let i = 0 ; i < products.length ; i++){
+    const product = products[i];
+    console.log(product);
+}
+
+
+// for(const product of products){
+//     console.log(product);
+// }
+
---
+++ Milestone -4/module 23- advance/guessingGame.js
@@ -0,0 +1,7 @@
+
+// Guessing game
+// Guess a number from 1 to 5 ;
+// Generate a random number between 1 to 5
+// If the guess number match random number ; than show massage won is lost
+// Run the game for 5 times
+// show the numbers of won is lost   
